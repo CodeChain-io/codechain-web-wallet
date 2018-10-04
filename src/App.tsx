c560dd20f333a643204ea7bda6d3ad7952b51fc9
Gilyoung <roadzero@kodebox.io>
2018-10-04 13:08:56 +0900

    Use sass as a css preprocessor

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import "./App.css";
 import logo from "./logo.svg";
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.tsx</code> and save to
+                    reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
