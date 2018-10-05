9e883dccef36ce72b1929ef3006345065d995cdf
Gilyoung <roadzero@kodebox.io>
2018-10-05 09:39:14 +0900

    Add a header and import the reactstrap

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,18 @@
 import * as React from "react";
 import { connect } from "react-redux";
+import {
+    Container,
+    Nav,
+    Navbar,
+    NavbarBrand,
+    NavItem,
+    NavLink
+} from "reactstrap";
 import { Dispatch } from "redux";
 import { Actions } from "../../actions";
 import { IRootState } from "../../reducers";
+import "./Header.css";
+import * as Logo from "./img/logo.png";
 
 interface IStateProps {
     isAuthenticated: boolean;
@@ -13,14 +23,37 @@ interface IDispatchProps {
 
 type IProps = IDispatchProps & IStateProps;
 class Header extends React.Component<IProps, any> {
+    public constructor(props: IProps) {
+        super(props);
+    }
     public render() {
         const { isAuthenticated } = this.props;
         return (
-            <div>
-                Header{" "}
-                {isAuthenticated && (
-                    <button onClick={this.handleLogout}>Logout</button>
-                )}
+            <div className="Header">
+                <Navbar color="dark" dark={true} expand="md">
+                    <Container>
+                        <NavbarBrand href="/">
+                            <div className="flex align-items-center">
+                                <img src={Logo} className="logo" />
+                                <span className="logo-text">
+                                    CodeChain Wallet
+                                </span>
+                            </div>
+                        </NavbarBrand>
+                        <Nav navbar={true} className="ml-auto">
+                            {isAuthenticated && (
+                                <NavItem className="ml-auto">
+                                    <NavLink
+                                        href="#"
+                                        onClick={this.handleLogout}
+                                    >
+                                        Logout
+                                    </NavLink>
+                                </NavItem>
+                            )}
+                        </Nav>
+                    </Container>
+                </Navbar>
             </div>
         );
     }
